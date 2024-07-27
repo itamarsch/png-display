99d452c985dfb6681b6e144d86e954187441391a
Itamar Schwartz <itamarschwartz10@gmail.com>
2024-07-28 02:54:16 +0300

    Filters

---
+++ src/filter_apply.rs
@@ -0,0 +1,117 @@
+#[derive(Debug)]
+enum PngFilterType {
+    None,
+    Sub,
+    Up,
+    Average,
+    Paeth,
+}
+
+fn paeth_predictor(a: u8, b: u8, c: u8) -> u8 {
+    let p = a as isize + b as isize - c as isize;
+    let pa = (p - a as isize).abs();
+    let pb = (p - b as isize).abs();
+    let pc = (p - c as isize).abs();
+
+    if pa <= pb && pa <= pc {
+        a
+    } else if pb <= pc {
+        b
+    } else {
+        c
+    }
+}
+
+pub fn decode_scanline(
+    filtered_scanline: &[u8],
+    previous_scanline: Option<&[u8]>,
+    bytes_per_pixel: usize,
+    decoded_scanline: &mut [u8],
+) {
+    let filter_type = match filtered_scanline[0] {
+        0 => PngFilterType::None,
+        1 => PngFilterType::Sub,
+        2 => PngFilterType::Up,
+        3 => PngFilterType::Average,
+        4 => PngFilterType::Paeth,
+        _ => panic!("Invalid filter! {:?}", filtered_scanline[0]),
+    };
+
+    match filter_type {
+        PngFilterType::None => {
+            // No filter; copy data directly
+            for (i, v) in (&filtered_scanline[1..]).iter().enumerate() {
+                decoded_scanline[i] = *v;
+            }
+        }
+        PngFilterType::Sub => {
+            // Sub filter; decode it
+            for i in 1..filtered_scanline.len() {
+                if i <= bytes_per_pixel {
+                    decoded_scanline[i - 1] = filtered_scanline[i];
+                } else {
+                    let decoded_byte = filtered_scanline[i]
+                        .wrapping_add(decoded_scanline[i - bytes_per_pixel - 1]);
+                    decoded_scanline[i - 1] = decoded_byte;
+                }
+            }
+        }
+        PngFilterType::Up => {
+            // Up filter; decode it
+            for i in 1..filtered_scanline.len() {
+                let above = if let Some(previous) = previous_scanline {
+                    previous[i - 1]
+                } else {
+                    0
+                };
+                let decoded_byte = filtered_scanline[i].wrapping_add(above);
+                decoded_scanline[i - 1] = decoded_byte;
+            }
+        }
+        PngFilterType::Average => {
+            // Average filter; decode it
+            for i in 1..filtered_scanline.len() {
+                let left = if i > bytes_per_pixel {
+                    decoded_scanline[i - bytes_per_pixel - 1]
+                } else {
+                    0
+                };
+                let above = if let Some(previous) = previous_scanline {
+                    previous[i - 1]
+                } else {
+                    0
+                };
+                let decoded_byte =
+                    filtered_scanline[i].wrapping_add(((left as u16 + above as u16) / 2) as u8);
+                decoded_scanline[i - 1] = decoded_byte;
+            }
+        }
+        PngFilterType::Paeth => {
+            // Paeth filter; decode it
+            for i in 1..filtered_scanline.len() {
+                let left = if i > bytes_per_pixel {
+                    decoded_scanline[i - bytes_per_pixel - 1]
+                } else {
+                    0
+                };
+                let above = if let Some(previous) = previous_scanline {
+                    previous[i - 1]
+                } else {
+                    0
+                };
+                let top_left = if let Some(previous) = previous_scanline {
+                    if i > bytes_per_pixel {
+                        previous[i - bytes_per_pixel - 1]
+                    } else {
+                        0
+                    }
+                } else {
+                    0
+                };
+                let decoded_byte =
+                    filtered_scanline[i].wrapping_add(paeth_predictor(left, above, top_left));
+                decoded_scanline[i - 1] = decoded_byte;
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use bitreader::BitReader;
 use chunk::PngChunk;
 use draw_image::display_image;
+use filter_apply::decode_scanline;
 use ihdr::IhdrChunk;
 use nom::{bytes::complete::tag, IResult};
 use std::fs::File;
@@ -10,10 +11,10 @@ use std::io::Read;
 use crate::chunk::parse_chunks;
 use crate::ihdr::parse_ihdr;
 use crate::plte::{parse_palette, Palette, PaletteEntries};
-use std::io::Write;
 
 mod chunk;
 mod draw_image;
+mod filter_apply;
 mod ihdr;
 mod plte;
 
@@ -53,7 +54,7 @@ fn parse_png(input: &[u8]) -> IResult<&[u8], (IhdrChunk, Vec<PngChunk>)> {
 }
 
 fn main() -> Result<()> {
-    let mut file = File::open("spiderman.png")?;
+    let mut file = File::open("NewTux.png")?;
     let mut buf = Vec::new();
     file.read_to_end(&mut buf)?;
 
@@ -64,7 +65,7 @@ fn main() -> Result<()> {
         .position(|elem| elem.chunk_type == "IDAT")
         .unwrap();
 
-    let mut pixels = vec![];
+    let mut pixels = vec![vec![(0, 0, 0, 0); ihdr.width as usize]; ihdr.height as usize];
     let data = chunks[idat..]
         .iter()
         .take_while(|elem| elem.chunk_type == IDAT)
@@ -75,37 +76,66 @@ fn main() -> Result<()> {
     let data = inflate::inflate_bytes_zlib(&data).unwrap();
     let mut bitreader = BitReader::new(&data[..]);
 
+    let mut prev_scanline = None;
+    let bit_depths_per_pixel = match &ihdr.color_type {
+        ihdr::ColorType::Grayscale => 1,
+        ihdr::ColorType::Rgb => 3,
+        ihdr::ColorType::Palette(_) => 1,
+        ihdr::ColorType::GrayscaleAlpha => 2,
+        ihdr::ColorType::Rgba => 4,
+    };
+    let bits_in_scanline = ihdr.bit_depth as u32 * bit_depths_per_pixel * ihdr.width;
+    let scanline_len = 1 + bits_in_scanline as usize / 8;
+
+    let mut scanline = vec![0; scanline_len];
+    let mut decoded = vec![0; scanline_len - 1];
     for i in 0..ihdr.height {
-        pixels.push(vec![]);
-        let filter = bitreader.read_u8(8).unwrap();
-        if filter != 0 {
-            panic!("Unknown filter: {:?}", filter);
-        }
-        for _ in 0..ihdr.width {
+        bitreader.read_u8_slice(&mut scanline)?;
+
+        decode_scanline(
+            &scanline[..],
+            prev_scanline.as_ref().map(|v: &Vec<u8>| &v[..]),
+            ((ihdr.bit_depth as f32 / 8.0) * bit_depths_per_pixel as f32).round() as usize,
+            &mut decoded,
+        );
+
+        let mut scanline_reader = BitReader::new(&decoded);
+
+        for j in 0..ihdr.width {
             match &ihdr.color_type {
                 ihdr::ColorType::Grayscale => todo!(),
                 ihdr::ColorType::Rgb => todo!(),
                 ihdr::ColorType::Palette(Palette {
                     entries: PaletteEntries::RGBA(values),
                 }) => {
-                    let index = bitreader.read_u8(ihdr.bit_depth)?;
+                    let index = scanline_reader.read_u8(ihdr.bit_depth)?;
                     let pixel = values[index as usize];
-                    pixels[i as usize].push(pixel);
+                    pixels[i as usize][j as usize] = pixel;
                 }
 
                 ihdr::ColorType::Palette(Palette {
                     entries: PaletteEntries::RGB(values),
                 }) => {
-                    let index = bitreader.read_u8(ihdr.bit_depth)?;
+                    let index = scanline_reader.read_u8(ihdr.bit_depth)?;
                     let (r, g, b) = values[index as usize];
-                    pixels[i as usize].push((r, g, b, 255));
+                    pixels[i as usize][j as usize] = (r, g, b, 255);
                 }
                 ihdr::ColorType::GrayscaleAlpha => todo!(),
-                ihdr::ColorType::Rgba => todo!(),
+                ihdr::ColorType::Rgba => {
+                    if ihdr.bit_depth == 8 {
+                        let r = scanline_reader.read_u8(8)?;
+                        let g = scanline_reader.read_u8(8)?;
+                        let b = scanline_reader.read_u8(8)?;
+                        let a = scanline_reader.read_u8(8)?;
+                        pixels[i as usize][j as usize] = (r, g, b, a);
+                    }
+                }
             }
         }
         let rem_bits = ihdr.width * ihdr.bit_depth as u32 % 8u32;
-        _ = bitreader.read_u8(rem_bits as u8);
+        _ = scanline_reader.read_u8(rem_bits as u8);
+
+        prev_scanline = Some(decoded.clone());
     }
 
     display_image(pixels);
