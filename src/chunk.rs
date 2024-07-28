4f86fa17fb6769e75fc4abbd4eeb1384c8a43e2a
Itamar Schwartz <itamarschwartz10@gmail.com>
2024-07-28 10:25:18 +0300

    Crc calculation

--- src/chunk.rs
+++ src/chunk.rs
@@ -4,7 +4,6 @@ use nom::{bytes::complete::take, number::complete::be_u32, IResult};
 pub struct PngChunk<'a> {
     pub chunk_type: &'a str,
     pub data: &'a [u8],
-    pub crc: u32,
 }
 
 fn parse_chunk(input: &[u8]) -> IResult<&[u8], PngChunk> {
@@ -12,6 +11,11 @@ fn parse_chunk(input: &[u8]) -> IResult<&[u8], PngChunk> {
     let (input, chunk_type) = take(4usize)(input)?;
     let (input, data) = take(length)(input)?;
     let (input, crc) = be_u32(input)?;
+    let calculated_crc = calculate_crc(chunk_type, data);
+    println!("Crc: {:?}, Calc: {:?}", crc, calculated_crc);
+    if crc != calculated_crc {
+        panic!("Invalid crc!!");
+    }
 
     let chunk_type = std::str::from_utf8(chunk_type).map_err(|_| {
         nom::Err::Failure(nom::error::Error::new(
@@ -20,14 +24,14 @@ fn parse_chunk(input: &[u8]) -> IResult<&[u8], PngChunk> {
         ))
     })?;
 
-    Ok((
-        input,
-        PngChunk {
-            chunk_type,
-            data,
-            crc,
-        },
-    ))
+    Ok((input, PngChunk { chunk_type, data }))
+}
+
+fn calculate_crc(chunk_type: &[u8], chunk_data: &[u8]) -> u32 {
+    let mut hasher = crc32fast::Hasher::new();
+    hasher.update(chunk_type);
+    hasher.update(chunk_data);
+    hasher.finalize()
 }
 
 pub fn parse_chunks(input: &[u8]) -> IResult<&[u8], Vec<PngChunk>> {
